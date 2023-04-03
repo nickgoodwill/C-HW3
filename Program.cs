d0c8055589e9478dc4ea7b3e7978e0686822401d
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-03 20:06:36 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+int N = GetInt();
+
+int GetInt()
+{
+    System.Console.WriteLine("Введите целое пятизначное число: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+int M = Last();
+
+int Last()
+{
+    int M = N % 10;
+    return M;
+}
+
+int H = First();
+
+int First()
+{
+    int H = N / 10000;
+    return H;
+}
+
+int K = Sec();
+
+int Sec()
+{
+    int K = N / 1000;
+    return K;
+}
+
+int B = Forth();
+
+int Forth()
+{
+    int B = N / 100;
+    return B;
+}
+
+if (M == H)
+{
+    int sec = K % 10;
+    int forth = B % 10;
+
+    if (K == B)
+    {
+        Console.WriteLine("Число полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число не полиндром");
+    }
+}
+else
+{   
+    Console.WriteLine("Число не полиндром");
+}
