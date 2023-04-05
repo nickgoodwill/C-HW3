70700088a76d13e20d6639400fb2a9248d958046
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-05 20:15:57 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -60,3 +60,28 @@ else
 {   
     Console.WriteLine("Число не полиндром");
 }
+
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+int Read(string coordinate)
+{
+    System.Console.WriteLine($"Введите координату {coordinate}:");
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+int x1 = Read("x1");
+int y1 = Read("y1");
+int z1 = Read("z1");
+int x2 = Read("x2");
+int y2 = Read("y2");
+int z2 = Read("z2");
+
+double vect = Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2)), 2);
+
+Console.WriteLine("Длина отрезка: ");
+Console.WriteLine(vect);
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
